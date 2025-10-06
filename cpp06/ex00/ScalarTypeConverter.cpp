5d2edb06e57359c4e2e243c4411f51c2f8df6f8e
Tomaz Ferreira De Lima <toferrei@c2r7s8.42lisboa.com>
2025-10-06 17:32:58 +0100

    ex00 no progress

--- cpp06/ex00/ScalarTypeConverter.cpp
+++ cpp06/ex00/ScalarTypeConverter.cpp
@@ -6,11 +6,12 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 15:29:52 by toferrei          #+#    #+#             */
-/*   Updated: 2025/10/02 16:03:00 by toferrei         ###   ########.fr       */
+/*   Updated: 2025/10/06 17:32:36 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarTypeConverter.hpp"
+#include <cstring>
 
 ScalarTypeConverter::ScalarTypeConverter()
 {
@@ -23,22 +24,26 @@ ScalarTypeConverter::~ScalarTypeConverter()
 
 }
 
-ScalarTypeConverter::ScalarTypeConverter()
-{
-
-}
-
 ScalarTypeConverter::ScalarTypeConverter(const ScalarTypeConverter &src)
 {
-
+	*this = src;
 }
 
 ScalarTypeConverter &ScalarTypeConverter::operator=(const ScalarTypeConverter &src)
 {
+	(void)src;
+	return (*this);
+}
 
+e_Type whatType(std::string)
+{
+	
 }
 
 void ScalarTypeConverter::convert(const std::string &str)
 {
+	e_Type type = whatType(str);
+	std::cout << str;
 
+	return ;
 }
--- cpp06/ex00/ScalarTypeConverter.hpp
+++ cpp06/ex00/ScalarTypeConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 15:30:18 by toferrei          #+#    #+#             */
-/*   Updated: 2025/10/02 15:37:55 by toferrei         ###   ########.fr       */
+/*   Updated: 2025/10/06 16:24:57 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,24 @@
 # define SCALARTYPECONVERTER_HPP
 
 #include <iostream>
+#include <cstdlib>
+#include <typeinfo>
+
+enum e_Type
+{
+	INVALID = -1,
+	SPECIAL = 0,
+	CHAR = 1,
+	INT = 2,
+	FLOAT = 3,
+	DOUBLE = 4
+};
 
 class ScalarTypeConverter
 {
 	private:
 		ScalarTypeConverter();
 		~ScalarTypeConverter();
-		ScalarTypeConverter();
 		ScalarTypeConverter(const ScalarTypeConverter &src);
 		ScalarTypeConverter &operator=(const ScalarTypeConverter &src);
 
