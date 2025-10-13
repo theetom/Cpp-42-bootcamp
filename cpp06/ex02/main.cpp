08efae98ff0a3489feb09def2f4ae3b61fa55cb0
Tomaz Ferreira De Lima <toferrei@c2r7s10.42lisboa.com>
2025-10-13 17:03:28 +0100

    ex02 done

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/13 16:33:25 by toferrei          #+#    #+#             */
+/*   Updated: 2025/10/13 16:58:34 by toferrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+
+#include <cstdlib>
+#include <iostream>
+
+Base *generate(void)
+{
+	std::srand((unsigned)(time(NULL)));
+	int i = std::rand() % 3;
+	std::cout << i << std::endl;
+	switch (i)
+	{
+		case 0:
+			return (new A);
+		case 1:
+			return (new B);
+		case 2:
+			return (new C);
+		default:
+			std::cout << "something else" << std::endl;
+			return (NULL);
+	}
+}
+
+void identify(Base *p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else if (dynamic_cast<C*>(p))
+		std::cout << "C" << std::endl;
+}
+
+void identify(Base &p)
+{
+	try
+	{
+		A& a = dynamic_cast<A&>(p);
+		(void)a;
+		std::cout << "A" << std::endl;
+	}
+	catch (std::exception& e)
+	{
+
+	}
+	try
+	{
+		B& b = dynamic_cast<B&>(p);
+		(void)b;
+		std::cout << "B" << std::endl;
+	}
+	catch (std::exception& e)
+	{
+
+	}
+	try
+	{
+		C& c = dynamic_cast<C&>(p);
+		(void)c;
+		std::cout << "C" << std::endl;
+	}
+	catch (std::exception& e)
+	{
+
+	}
+}
+
+int main()
+{
+	Base *ptr = generate();
+
+	identify(ptr);
+	identify(*ptr);
+
+	delete ptr;
+	return (0);
+}
