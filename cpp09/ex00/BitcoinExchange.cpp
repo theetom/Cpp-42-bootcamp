5cc25d23c7626572d1e1a926c6a29cdebc6876b1
Tomaz Ferreira De Lima <toferrei@c2r2s7.42lisboa.com>
2025-10-30 17:45:54 +0000

    final antes casa

--- cpp09/ex00/BitcoinExchange.cpp
+++ cpp09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 15:00:14 by toferrei          #+#    #+#             */
-/*   Updated: 2025/10/24 17:10:04 by toferrei         ###   ########.fr       */
+/*   Updated: 2025/10/30 17:40:22 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,38 +22,10 @@ BitcoinExchange::~BitcoinExchange()
 
 }
 
-int datetoint(std::string date)
-{
-	date = date.substr();
-	return (std::atoi(date.c_str()));
-}
-
-BitcoinExchange::BitcoinExchange(std::fstream *inputFile)
-{
-	std::string strtmp, date, btcValue;
-	char *errCheck;
-	int i = 0;
-	while (getline(*inputFile, strtmp) && i < 2)
-	{
-		if (strtmp != "date,exchange_rate")
-		{
-			int key = std::atoi(date.c_str());
-			float value = std::strtof(btcValue.c_str(), &errCheck);
-			if (btcValue == errCheck)
-				throw FailedConvertion();
-			this->_dataBase.insert(std::make_pair(key, value));
-			std::cout << errCheck << std::endl;
-			std::cout << strtmp << std::endl;
-		}
-		i++;
-	}
-	
-
-}
 
 BitcoinExchange::BitcoinExchange(const BitcoinExchange &src): _dataBase(src._dataBase)
 {
-
+	
 }
 
 BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &src)
@@ -65,7 +37,104 @@ BitcoinExchange &BitcoinExchange::operator=(const BitcoinExchange &src)
 	return (*this);
 }
 
+std::string cleanInput(std::string input, char c)
+{
+	for (std::string::iterator it = input.begin(); it < input.end(); it++)
+	{
+		if (*it == c)
+		{
+			input.erase(it);
+			it--;
+		}
+	}
+	return (input);
+}
+
+int datetoint(std::string date)
+{
+	date = cleanInput(date, '-');
+	return (std::atoi(date.c_str()));
+}
+
+BitcoinExchange::BitcoinExchange(std::fstream *inputFile)
+{
+	std::string strtmp, btcValue;
+	float value;
+	char *errCheck;
+	int key;
+	while (std::getline(*inputFile, strtmp))
+	{
+		if (strtmp != "date,exchange_rate")
+		{
+			key = datetoint(strtmp.substr(0, strtmp.find(",")));
+			btcValue = strtmp.substr(strtmp.find(",") + 1);
+			value = std::strtof(btcValue.c_str(), &errCheck);
+			if (btcValue == errCheck)
+				throw FailedConvertion();
+			this->_dataBase.insert(std::make_pair(key, value));
+		}
+	}
+}
+
+void isInputValid(std::string input)
+{
+
+}
+
+void isDateValid(std::string date)
+{
+	// size_t year, month, day;
+	(void) date;
+
+}
+
+void isValueValid()
+{
+
+}
+
+void BitcoinExchange::comparator(std::fstream *inputFile, char *fileName) const
+{
+	std::string strtmp;
+	int i = 0;
+	while (std::getline(*inputFile, strtmp))
+	{
+		if (strtmp != "date | value")
+		{
+			try
+			{
+				strtmp = cleanInput(strtmp, ' ');
+				isInputValid(strtmp);
+				std::cout << "ola" << std::endl;
+				isDateValid(strtmp);
+				isValueValid();
+			}
+			catch (std::exception &e)
+			{
+				std::cerr << e.what() << std::endl;
+				std::cout << "In file " << fileName << "line " << i << std::endl;
+			}
+		}
+		i++;
+	}
+}
+
 const char *BitcoinExchange::FailedConvertion::what() const throw()
 {
 	return ("Error: Failed convertion");
+}
+
+const char *BitcoinExchange::InvalidInput::what() const throw()
+{
+	return ("Error: Invalid input");
+}
+
+const char *BitcoinExchange::InvalidDate::what() const throw()
+{
+	return ("Error: Invalid date");
+}
+
+const char *BitcoinExchange::InvalidValue::what() const throw()
+{
+	return ("Error: Invalid Value");
 }
--- cpp09/ex00/BitcoinExchange.hpp
+++ cpp09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: toferrei <toferrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 11:56:09 by toferrei          #+#    #+#             */
-/*   Updated: 2025/10/24 16:45:04 by toferrei         ###   ########.fr       */
+/*   Updated: 2025/10/30 17:17:49 by toferrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 # define BITCOINEXCHANGE_HPP
 
 #include <map>
+#include <string>
 #include <ctime>
 #include <fstream>
 #include <iostream>
@@ -31,7 +32,27 @@ class BitcoinExchange
 		BitcoinExchange(const BitcoinExchange &src);
 		BitcoinExchange &operator=(const BitcoinExchange &src);
 
+		void comparator(std::fstream *inputFile, char *fileName) const;
+
 		class FailedConvertion : public std::exception
+		{
+			public :
+				virtual const char *what() const throw();
+		};
+
+		class InvalidInput : public std::exception
+		{
+			public :
+				virtual const char *what() const throw();
+		};
+
+		class InvalidDate : public std::exception
+		{
+			public :
+				virtual const char *what() const throw();
+		};
+
+		class InvalidValue : public std::exception
 		{
 			public :
 				virtual const char *what() const throw();
